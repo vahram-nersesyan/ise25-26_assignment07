b279e870a453cbd4f698170aa27d218f2ee38ff5
Vahram Nersesyan <vahnersesyan@gmail.com>
2025-12-04 16:33:49 +0100

    Implement Review domain model

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
@@ -1,8 +1,11 @@
 package de.seuhd.campuscoffee.domain.model.objects;
 
+import jakarta.validation.constraints.Null;
 import lombok.Builder;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
+
+import java.time.LocalDateTime;
 
 /**
  * Domain record that stores a review for a point of sale.
@@ -12,6 +15,11 @@ import org.jspecify.annotations.Nullable;
 public record Review(
         @Nullable Long id, // null when the review has not been created yet
         //TODO: Implement review domain model.
+        @Nullable LocalDateTime createdAt,
+        @Nullable LocalDateTime updatedAt,
+        @NonNull Pos pos,
+        @NonNull User author,
+        @NonNull String review,
         @NonNull Integer approvalCount, // is updated by the domain module
         @NonNull Boolean approved // is determined by the domain module
 ) implements DomainModel<Long> {
