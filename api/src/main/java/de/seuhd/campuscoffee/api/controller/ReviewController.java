3d96ece8e927b84071139fb42995cc8cbef1468f
Vahram Nersesyan <vahnersesyan@gmail.com>
2025-12-06 16:23:20 +0100

    Implement ReviewController

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
@@ -1,12 +1,19 @@
 package de.seuhd.campuscoffee.api.controller;
 
 import de.seuhd.campuscoffee.api.dtos.ReviewDto;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
 import de.seuhd.campuscoffee.api.mapper.DtoMapper;
+import de.seuhd.campuscoffee.api.mapper.ReviewDtoMapper;
+import de.seuhd.campuscoffee.api.mapper.UserDtoMapper;
 import de.seuhd.campuscoffee.api.openapi.CrudOperation;
 import de.seuhd.campuscoffee.domain.model.objects.Review;
 import de.seuhd.campuscoffee.domain.ports.api.CrudService;
+import de.seuhd.campuscoffee.domain.ports.api.ReviewService;
+import de.seuhd.campuscoffee.domain.ports.api.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
@@ -18,6 +25,7 @@ import java.util.List;
 
 import static de.seuhd.campuscoffee.api.openapi.Operation.*;
 import static de.seuhd.campuscoffee.api.openapi.Resource.REVIEW;
+import static de.seuhd.campuscoffee.api.openapi.Resource.USER;
 
 /**
  * Controller for handling reviews for POS, authored by users.
@@ -28,17 +36,19 @@ import static de.seuhd.campuscoffee.api.openapi.Resource.REVIEW;
 @Slf4j
 @RequiredArgsConstructor
 public class ReviewController extends CrudController<Review, ReviewDto, Long> {
+    private final ReviewService reviewService;
+    private final ReviewDtoMapper reviewDtoMapper;
 
     // TODO: Correctly implement the service() and mapper() methods. Note the IntelliJ warning resulting from the @NonNull annotation.
 
     @Override
     protected @NonNull CrudService<Review, Long> service() {
-        return null;
+        return reviewService;
     }
 
     @Override
     protected @NonNull DtoMapper<Review, ReviewDto> mapper() {
-        return null;
+        return reviewDtoMapper;
     }
 
     @Operation
@@ -49,4 +59,67 @@ public class ReviewController extends CrudController<Review, ReviewDto, Long> {
     }
 
     // TODO: Implement the missing methods/endpoints.
+    @Operation
+    @CrudOperation(operation=GET_BY_ID, resource=REVIEW)
+    @GetMapping("/{id}")
+    public @NonNull ResponseEntity<ReviewDto> getById(
+            @Parameter(description="Unique identifier of the review to retrieve.", required=true)
+            @PathVariable Long id) {
+        return super.getById(id);
+    }
+
+    @Operation
+    @CrudOperation(operation=CREATE, resource=REVIEW)
+    @PostMapping("")
+    public @NonNull ResponseEntity<ReviewDto> create(
+            @Parameter(description="Data of the review to create.", required=true)
+            @RequestBody @Valid ReviewDto reviewDto) {
+        return super.create(reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=UPDATE, resource=REVIEW)
+    @PutMapping("/{id}")
+    public @NonNull ResponseEntity<ReviewDto> update(
+            @Parameter(description="Unique identifier of the review to update.", required=true)
+            @PathVariable Long id,
+            @Parameter(description="Data of the review to update.", required=true)
+            @RequestBody @Valid ReviewDto reviewDto) {
+        return super.update(id, reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=DELETE, resource=REVIEW)
+    @DeleteMapping("/{id}")
+    public @NonNull ResponseEntity<Void> delete(
+            @Parameter(description="Unique identifier of the review to delete.", required=true)
+            @PathVariable Long id) {
+        return super.delete(id);
+    }
+
+    @Operation
+    @CrudOperation(operation=FILTER, resource=REVIEW)
+    @GetMapping("/filter")
+    public ResponseEntity<List<ReviewDto>> filter(
+            @RequestParam ("pos_id") Long posId,
+            @RequestParam ("approved") Boolean approved){
+        List<Review> reviews = reviewService.filter(posId, approved);
+        List<ReviewDto> dtos = reviews.stream()
+                .map(reviewDtoMapper::fromDomain)
+                .toList();
+
+        return ResponseEntity.ok(dtos);
+    }
+
+    @Operation(summary = "Approve a review.",
+            description = "Approves a review by ID. If the approval threshold is reached, the review becomes publicly visible.")
+    @PostMapping("/{id}/approve")
+    public ResponseEntity<ReviewDto> approve (
+            @PathVariable Long id,
+            @RequestParam ("user_id") Long userId
+    ){
+        Review review = service().getById(id);
+        Review approvedReview = reviewService.approve(review, userId);
+        return ResponseEntity.ok(reviewDtoMapper.fromDomain(approvedReview));
+    }
 }
